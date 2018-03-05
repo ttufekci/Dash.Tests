6190e7cb17094277214d564dbf14fa96a0186c3a
talattufekci <talattufekci@gmail.com>
2018-03-05 17:38:58 +0300

    findbestmatch table with prefix

--- UtilTest.cs
+++ UtilTest.cs
@@ -8,9 +8,20 @@ namespace Dash.Tests
     public class UtilTest
     {
         [Fact]
-        public void TestFindMatch()
+        public void TestFindMatchTableWithoutPrefix()
         {
             var tableName = "PERSON";
+            var sequenceList = new List<string>();
+            sequenceList.Add("SEQ_TASK");
+            sequenceList.Add("SEQ_PERSON");
+            var seqTask = Util.FindBestMatch(tableName, sequenceList);
+            Assert.Equal("SEQ_PERSON", seqTask);
+        }
+
+        [Fact]
+        public void TestFindMatchTableWithPrefix()
+        {
+            var tableName = "SYS_PERSON";
             var sequenceList = new List<string>();
             sequenceList.Add("SEQ_TASK");
             sequenceList.Add("SEQ_PERSON");
